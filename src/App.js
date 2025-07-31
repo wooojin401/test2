766b3b90fccf5f0aec5819959532c6925b0355e2
poroli0529 <kafaprl@gmail.com>
2025-07-31 10:40:08 +0900

    새로다시올림

--- src/App.js
+++ src/App.js
@@ -5,15 +5,19 @@ import Home from "./pages/Home";
 import Write from "./pages/Write";
 import Detail from "./pages/Detail";
 import Edit from "./pages/Edit";
+import Header from "./pages/layout/Header";
+import Footer from "./pages/layout/Footer";
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/write" element={<Write />} />
         <Route path="/post/:id" element={<Detail />} />
         <Route path="/edit" element={<Edit />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
