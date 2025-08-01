a7fb03144439e0a92a90dbf7e212ff1f3eecfcd9
poroli0529 <kafaprl@gmail.com>
2025-08-01 13:32:39 +0900

    0801최종본

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import Detail from "./pages/Detail";
 import Edit from "./pages/Edit";
 import Header from "./pages/layout/Header";
 import Footer from "./pages/layout/Footer";
-import Sidebar from "./pages/layout/Sidebar"; // ✅ 추가
+import Sidebar from "./pages/layout/Sidebar";
+import CodePage from "./pages/code_function/CodePage";
 import './App.css'; 
 import Login from "./pages/Login";
 
@@ -32,15 +33,16 @@ function App() {
   };
   return (
     <BrowserRouter>
-      <Header />
+      <Header scrollIntoView = {scrollIntoView} scrollIntoUse = {scrollIntoUse} scrollIntoWhy = {scrollIntoWhy} />
       <div className="main_layout">
         <div className="content">
           <Routes>
-            <Route path="/" element={<Home />} />
+            <Route path="/" element={<Home aboutRef = {aboutRef} howUseRef = {howUseRef} whyUseRef={whyUseRef} />} />
             <Route path="/write" element={<Write />} />
             <Route path="/post/:id" element={<Detail />} />
             <Route path="/login" element={<Login />} />
             <Route path="/edit/:id" element={<Edit />} />
+            <Route path="/CodePage" element={<CodePage/>}/>
           </Routes>
         </div>
       </div>
